715dccc42b6f482ca099d6d4a72b33ccce04bef7
Alyssa Jane <xcthaeh@gmail.com>
2019-06-17 16:29:30 -0400

    created a succeed function

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -4,17 +4,45 @@ module.exports = {
   repair,
   get
 };
-//- a `success(item)` method that accepts an `item` object and **returns a new item** 
+//- a `success(item)` method that accepts an `item` object and **returns a new item**
 //  object modified according to the rules defined by the client for enhancement success.
 //- The item's enhancement increases by 1.
 //- If the item enhancement level is 20, the enhancement level is not changed.
 //- The durability of the item is not changed.
 
 function succeed(item) {
-  return { ...item };
+  
+  if (item.enhancement >= 0 && item.enhancement <= 15) {
+    item.enhancement += 1;
+    item.name = `[+${item.enhancement}] ${item.origName}`;
+  } else if (item.enhancement === 15) {
+    item.enhancement += 1;
+    item.name = `[PRI] ${item.origName}`;
+  } else if (item.enhancement === 16) {
+    item.enhancement += 1;
+    item.name = `[DUO] ${item.origName}`;
+  } else if (item.enhancement === 17) {
+    item.enhancement += 1;
+    item.name = `[TRI] ${item.origName}`;
+  } else if (item.enhancement === 18) {
+    item.enhancement += 1;
+    item.name = `[TET] ${item.origName}`;
+  } else if (item.enhancement === 19) {
+    item.enhancement += 1;
+    item.name = `[PEN] ${item.origName}`;
+  }
+
+  if (
+    (item.enhancement <= 14 && item.durability < 25) ||
+    (item.enhancement >= 15 && item.durability < 10)
+  ) {
+    throw new Error("Could not enhance. Durability level too low.");
+  }
+
+  return item;
 }
 
-//- a `fail(item)` method that accepts an `item` object and **returns a new item** object modified 
+//- a `fail(item)` method that accepts an `item` object and **returns a new item** object modified
 //according to the rules defined by the client for enhancement failure.
 //- If the item's enhancement is less than 15, the durability of the item is decreased by 5.
 //- If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
@@ -24,8 +52,7 @@ function fail(item) {
   return { ...item };
 }
 
-
-//- a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. 
+//- a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100.
 //This method is the simplest of the three and would be a **good starting point** on this project.
 
 function repair(item) {
