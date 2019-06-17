93f42b1c2d68758cd643fa680cac34cf5f79c5d0
Alyssa Jane <xcthaeh@gmail.com>
2019-06-17 17:05:31 -0400

    created a list of armors

---
+++ garb/armor.js
@@ -0,0 +1,92 @@
+const daedric = {
+  origName: "daedric",
+  name: "daedric",
+  type: "armor",
+  durability: 100,
+  enhancement: 17
+};
+
+const dragonscale = {
+  origName: "dragonscale",
+  name: "dragonscale",
+  type: "armor",
+  durability: 100,
+  enhancement: 7
+};
+
+const miraaks = {
+  origName: "miraaks",
+  name: "miraaks",
+  type: "armor",
+  durability: 100,
+  enhancement: 0
+};
+
+const ahzidals = {
+  origName: "ahzidals",
+  name: "ahzidals",
+  type: "armor",
+  durability: 100,
+  enhancement: 0
+};
+
+const deathbrand = {
+  origName: "deathbrand",
+  name: "deathbrand",
+  type: "armor",
+  durability: 100,
+  enhancement: 15
+};
+
+const nightingale = {
+  origName: "nightingale",
+  name: "nightingale",
+  type: "armor",
+  durability: 100,
+  enhancement: 0
+};
+
+const ancient_shrouded = {
+  origName: "ancient_shrouded",
+  name: "ancient_shrouded",
+  type: "armor",
+  durability: 100,
+  enhancement: 8
+};
+
+const archmages = {
+  origName: "archmages",
+  name: "archmages",
+  type: "armor",
+  durability: 100,
+  enhancement: 6
+};
+
+const guild_masters = {
+  origName: "guild_masters",
+  name: "guild_masters",
+  type: "armor",
+  durability: 100,
+  enhancement: 0
+};
+
+const vampire_royal = {
+  origName: "vampire_royal",
+  name: "vampire_royal",
+  type: "armor",
+  durability: 100,
+  enhancement: 10
+};
+
+module.exports = {
+  daedric,
+  dragonscale,
+  miraaks,
+  ahzidals,
+  deathbrand,
+  nightingale,
+  ancient_shrouded,
+  archmages,
+  guild_masters,
+  vampire_royal
+};
