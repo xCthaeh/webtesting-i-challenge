f83008b190876960197e96e086544c79e2427acc
Alyssa Jane <xcthaeh@gmail.com>
2019-06-17 17:36:51 -0400

    added a test for success method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -26,3 +26,55 @@ const {
   guild_masters,
   vampire_royal
 } = require("../garb/armor");
+
+describe("GAME TEST SUITE", () => {
+  describe("SUCCESS METHOD", () => {
+    const expectedArmor = {
+      origName: "ahzidals",
+      name: "[+1] ahzidals",
+      type: "armor",
+      durability: 100,
+      enhancement: 1
+    };
+
+    test("Increase armor enhancement by 1", () => {
+      expect(enhance.success(ahzidals)).toEqual(expectedArmor);
+    });
+
+    const expectedArmorAbove15 = {
+      origName: "daedric",
+      name: "daedric",
+      type: "armor",
+      durability: 100,
+      enhancement: 17
+    };
+
+    test("Increased armor enhancement by 1 above 15", () => {
+      expect(enhance.success(daedric)).toEqual(expectedArmorAbove15);
+    });
+
+    const expectedWeapon = {
+      origName: "daedric_dagger",
+      name: "[+1] daedric_dagger",
+      type: "weapon",
+      durability: 100,
+      enhancement: 1
+    };
+
+    test("Increase weapons enhancement by 1", () => {
+      expect(enhance.success(daedric_dagger)).toEqual(expectedWeapon);
+    });
+
+    const expectedWeaponAbove15 = {
+      origName: "daedric_warhammer",
+      name: "daedric_warhammer",
+      type: "weapon",
+      durability: 100,
+      enhancement: 19
+    };
+
+    test("Increase weapon enhancement by 1 above 15", () => {
+      expect(enhance.success(daedric_warhammer)).toEqual(expectedWeaponAbove15);
+    });
+  });
+});
