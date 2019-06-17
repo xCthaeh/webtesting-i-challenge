fe0377c6942ccd86cfefad9e7f4c350f44b76a64
Alyssa Jane <xcthaeh@gmail.com>
2019-06-17 17:11:43 -0400

    added a require for both weapons and armor into test file

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,28 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+const {
+  windshear,
+  chillrend,
+  dragonbane,
+  bloodscythe,
+  daedric_dagger,
+  auriels_bow,
+  harkons_sword,
+  daedric_warhammer,
+  dwarven_crossbow,
+  mace_of_molagbal
+} = require("../garb/weapons");
+
+const {
+  daedric,
+  dragonscale,
+  miraaks,
+  ahzidals,
+  deathbrand,
+  nightingale,
+  ancient_shrouded,
+  archmages,
+  guild_masters,
+  vampire_royal
+} = require("../garb/armor");
